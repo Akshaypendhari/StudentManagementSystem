01d803f077e96e9145f4c4f95050f7ac5b12a1de
akshayPendhari <109335000+Akshaypendhari@users.noreply.github.com>
2022-11-18 19:52:25 +0530

    Add files via upload

---
+++ CP.java
@@ -0,0 +1,32 @@
+package studentManagementSystem;
+import java.sql.*;
+
+import java.sql.DriverManager;
+
+public class CP {
+	static Connection con ;
+	
+public  static Connection  createC() {
+	
+	try {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		String url = "jdbc:mysql://localhost:3306/studentmanage";
+		String uname= "root";
+		String pass ="root";
+		
+				
+		 con = DriverManager.getConnection(url,uname,pass);
+		
+		
+	} catch (Exception e) {
+		// TODO: handle exception
+		e.printStackTrace();
+	}
+	return con;
+	
+	
+}
+
+}
